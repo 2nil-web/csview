066db6edb9836b9446925b1299c6234380404377
Denis LALANNE SII <denis.lalanne@sii.fr>
2022-09-28 13:03:11 +0200

    Checked in by Denis LALANNE SII from FRTLS-2438 on mer. 28 sept. 2022 13:03:11

--- nl.cpp
+++ nl.cpp
@@ -9,74 +9,25 @@ std::string prog_basename;
 
 // tail, see : https://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
 
-void tail1(std::string fn) {
-  std::fstream fin(fn, std::ios::in);
-	
+void tail(std::string fn, int n) {
+  std::ifstream fin(fn);
+
   if (fin.is_open()) {
-    int n=0;
-    int pos=-1;
-    char ch;
-    std::string str;
-    fin.seekg(0L, std::ios::end);
-    
-    while(n != 5) {
-      fin.seekg(pos, std::ios::cur);
-      pos--;
-      
-      fin.get(ch);
-      if (ch=='\n') n++;
+    fin.seekg(-1, std::ios_base::end);
+
+    while (n && fin.good()) {
+      fin.seekg(-1, std::ios_base::cur);
+      if (fin.peek() == '\n') n--;
     }
 
-    std::cout <<n << std::endl;
-    
-    while(fin.eof()) {
-      std::getline(fin, str);
-      std::cout << str << std::endl;
-    }
-	}
-}
-
-void tail2(std::string filename) {
-  std::ifstream fs(filename);
-
-  if (fs.is_open()) {
-    //Got to the last character before EOF
-    fs.seekg(-1, std::ios_base::end);
-
-    if (fs.peek() == '\n') {
-      //Start searching for \n occurrences
-      fs.seekg(-1, std::ios_base::cur);
-      int i = fs.tellg();
-      for(;i > 0; i--) {
-        if (fs.peek() == '\n') {
-          //Found
-          fs.get();
-          break;
-        }
-        //Move one character back
-        fs.seekg(i, std::ios_base::beg);
-      }
-    }
-
-    std::string lastline;
-    getline(fs, lastline);
-    std::cout << lastline << std::endl;
-  } else {
-    std::cout << "Could not find end line character" << std::endl;
+    fin.seekg(1, std::ios_base::cur);
+    std::string ln;
+    while (getline(fin, ln)) std::cout << ln << std::endl;
+    fin.close();
   }
 }
 
 unsigned int nl(std::string fn) {
-  unsigned int _nl = 0;
-  FILE *fp=fopen(fn.c_str(), "r");
-  int ch;
-
-  while (EOF != (ch=getc(fp)))
-      if ('\n' == ch) ++_nl;
-  return _nl;
-}
-
-unsigned int nl_cpp(std::string fn) {
   std::string ln;
   unsigned int _nl=0;
   std::ifstream fp(fn);
@@ -93,9 +44,8 @@ int main(int argc, char *argv[]) {
   prog_basename=std::filesystem::path(args[0]).stem().string();
   args.erase(args.begin());
 
-  for (auto fn:args) {
-    std::cout << fn << ':' << nl(fn) << std::endl;
-  }
+  if (args.size() == 1) tail(args[1], 10); 
+  if (args.size() >= 2) tail(args[1], std::stoi(args[0])); 
   return 0;
 }
 
