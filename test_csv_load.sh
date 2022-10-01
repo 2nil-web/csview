bd872879a1cc3f6f12ed312376c0feafd039436c
denis <denis.lalanne@laposte.net>
2022-10-01 18:55:53 +0200

    Checked in by root with git user denis and git email denis.lalanne@laposte.net from pc-tv on sam. 01 oct. 2022 18:55:53 CEST

--- test_csv_load.sh
+++ test_csv_load.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./mkcsv.sh 10000 10   0 >file_10000x10x000.csv
 ./mkcsv.sh 10000 10   8 >file_10000x10x008.csv
 ./mkcsv.sh 10000 10  16 >file_10000x10x016.csv
 ./mkcsv.sh 10000 10  32 >file_10000x10x032.csv
@@ -7,10 +8,10 @@
 ./mkcsv.sh 10000 10 128 >file_10000x10x128.csv
 ./mkcsv.sh 10000 10 255 >file_10000x10x255.csv
 
-for i in file_10*.csv; do
-  echo
-  ./mywc "$i"; time ./mytail 0 "$i" | tee -a toto.txt | grep -E '^real|^user|^sys'
+for i in file_10000*.csv; do
+	( ./mywc "$i"; time ./mytail 0 "$i" ) >>res.txt
 done
 
-rm toto.txt
+grep -E '^file_10000|^real|^user|^sys' res.txt
+#rm res.txt
 
