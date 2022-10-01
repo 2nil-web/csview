f292aa1d18683512fa48e94f47596d45b5342685
2nil <casalalanne@free.fr>
2022-10-01 18:26:45 +0200

    Checked in by casal from PC-Denis on sam.  1 oct. 2022 18:26:44

--- mytail.cpp
+++ mytail.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
+#include <algorithm>
 
 std::vector<std::string> args, envs;
 std::string prog_basename;
@@ -13,18 +14,25 @@ void tail(std::string fn, int n) {
   std::ifstream fin(fn);
 
   if (fin.is_open()) {
-    fin.seekg(-1, std::ios_base::end);
+    if (n > 0) {
+      fin.seekg(-1, std::ios_base::end);
 
-    while (n && fin.good()) {
-      fin.seekg(-1, std::ios_base::cur);
-      if (fin.peek() == '\n') n--;
+      while (n && fin.good()) {
+        fin.seekg(-1, std::ios_base::cur);
+        if (fin.peek() == '\n') n--;
+      }
+
+      fin.seekg(1, std::ios_base::cur);
     }
 
-    fin.seekg(1, std::ios_base::cur);
     std::string ln;
     while (getline(fin, ln)) std::cout << ln << std::endl;
     fin.close();
   }
+}
+
+bool is_positive_integer(const std::string &s) {
+  return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
 }
 
 int main(int argc, char *argv[]) { 
@@ -32,8 +40,17 @@ int main(int argc, char *argv[]) {
   prog_basename=std::filesystem::path(args[0]).stem().string();
   args.erase(args.begin());
 
-  if (args.size() == 1) tail(args[0], 10); 
-  if (args.size() > 1) tail(args[1], std::stoi(args[0])); 
+  int nl=10;
+
+  if (args.size() == 1) tail(args[0], nl);
+  else if (args.size() > 1) {
+    if (is_positive_integer(args[0])) {
+      nl=std::stoi(args[0]);
+      args.erase(args.begin());
+    }
+
+    for(auto f: args) tail(f, nl);
+  }
   return 0;
 }
 
--- mywc.cpp
+++ mywc.cpp
@@ -7,9 +7,10 @@
 std::vector<std::string> args, envs;
 std::string prog_basename;
 
-unsigned int nl(std::string fn) {
+// Compute the number of lines of a files
+int nl(std::string fn) {
   std::string ln;
-  unsigned int _nl=0;
+  int _nl=0;
   std::ifstream fp(fn);
 
   if (fp.is_open()) {
@@ -19,11 +20,20 @@ unsigned int nl(std::string fn) {
   return _nl;
 }
 
+// Compute the density of a file.
+// The density is the average of characters by lines.
+// This parameter may have an influence during the load of a text file
+uintmax_t density (std::string fn) {
+  // Get file size
+  std::uintmax_t fs=std::filesystem::file_size(fn);
+  return fs/nl(fn);
+}
+
 int main(int argc, char *argv[]) { 
   args=std::vector<std::string>(argv, argv + argc);
   prog_basename=std::filesystem::path(args[0]).stem().string();
   args.erase(args.begin());
-  for(auto f: args) std::cout << nl(f) << std::endl;
+  for(auto f: args) std::cout << f << ':' << nl(f) << " lines, " << density(f) << " chars per line." << std::endl;
   return 0;
 }
 
