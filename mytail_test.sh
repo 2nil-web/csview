bf388a733debda3ffcd5549d3f25eb5461c21988
2nil <casalalanne@free.fr>
2022-10-06 18:36:30 +0200

    Checked in by casal from PC-Denis on jeu.  6 oct. 2022 18:36:30

--- mytail_test.sh
+++ mytail_test.sh
@@ -82,18 +82,5 @@ done
   fi
 
 
-#cat test_res.txt
-
-
-
-let ts_min=$(date -d '2020-01-01' '+%s')
-let ts_max=$(date '+%s')
-let delta=(($ts_max-$ts_min)
-echo "ts min=$ts_min, ts max=$ts_max, delta=$delta"
-
-for i in {1..10}; do
-  ts=$(($(date -d '2020-01-01' '+%s') + 1 + $RANDOM % $ts_max))
-  dt=$(date -d "@$ts" '+%Y-%m-%dT%H:%M:%S%z')
-  echo "$ts == $dt"
-done
+cat test_res.txt
 
